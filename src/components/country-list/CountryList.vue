985b571f44d9807a12b9a12d8a3e3c013560ff78
liyn <liyn@myibc.net>
2023-06-05 14:20:27 +0800

    解决中文搜索无效问题

--- src/components/country-list/CountryList.vue
+++ src/components/country-list/CountryList.vue
@@ -89,8 +89,9 @@ export default {
       countries =  countries.filter(item => {
         let reg = new RegExp(searchText, 'gi');
         // console.log('reg',reg);
-        let nameFlag = reg.test(item.name || item.nameCN);
-        if(nameFlag){
+        let nameFlag = reg.test(item.name );
+        let nameFlag2 = reg.test(item.nameCN);
+        if(nameFlag || nameFlag2){
           return true;
         }
         let dialCodeFlag = reg.test(item.dialCode);
